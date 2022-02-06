aa145f487afb5dd3c13a6c80e704b792e3c90988
learningnoobi <fanime492@gmail.com>
2022-02-06 21:49:14 +0545

    new changes

--- static/js/bingo.js
+++ static/js/bingo.js
@@ -1,29 +1,6 @@
  var grid = document.querySelector(".grid");
 
-      const urls = "ws://127.0.0.1:8000/ws/clicked"+window.location.pathname;
-      const ws = new WebSocket(urls);
-      const addmearr = [];
-      ws.onopen = function (e) {
-        console.log("connection open");
-      };
-      ws.onclose = function (e) {
-        console.log("closed");
-      };
-      ws.onerror = function (e) {
-        console.log("error is ", e);
-      };
-      ws.onmessage = function (e) {
-        const data = JSON.parse(e.data);
-       // console.log(data);
-        const clickedDiv = document.querySelector(
-          `[data-innernum='${data.dataset}']`
-        );
-        addmearr.push(data.dataset)
-        console.log(addmearr)
-       
-        clickedDiv.classList.add("clicked");
-      };
-
+     
       const items = [...document.querySelector(".grid").children];
       const bingodiv = document.querySelector("#bingodiv");
       let keysArr = [];
