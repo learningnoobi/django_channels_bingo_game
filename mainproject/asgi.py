2b26a8246732c9c816cc279b26f31d64900b0ccd
learningnoobi <fanime492@gmail.com>
2022-02-07 22:08:57 +0545

    bug fix

--- mainproject/asgi.py
+++ mainproject/asgi.py
@@ -5,7 +5,7 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 import bingo.routing
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mainproject.settings")
 
 application = ProtocolTypeRouter({
   "http": get_asgi_application(),
--- mainproject/settings.py
+++ mainproject/settings.py
@@ -23,14 +23,15 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    'channels',
+    'bingo',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'channels',
-    'bingo'
+    
 ]
 
 MIDDLEWARE = [
