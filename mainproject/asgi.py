257c61ef3c657655d935094045df2d20920e35c6
learningnoobi <fanime492@gmail.com>
2022-02-07 22:20:05 +0545

    heroku setup

--- mainproject/asgi.py
+++ mainproject/asgi.py
@@ -1,11 +1,14 @@
 import os
-
+import django
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mainproject.settings")
+django.setup()
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 import bingo.routing
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mainproject.settings")
+
+
 
 application = ProtocolTypeRouter({
   "http": get_asgi_application(),
