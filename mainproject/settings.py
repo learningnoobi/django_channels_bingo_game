f628ac19130f38911b3db8fdb698619751b8556f
learningnoobi <fanime492@gmail.com>
2022-02-18 00:13:54 +0545

    https redirect

--- mainproject/settings.py
+++ mainproject/settings.py
@@ -124,4 +124,9 @@ STATICFILES_DIRS = (str(BASE_DIR.joinpath('static')),)
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 django_heroku.settings(locals())
+if not DEBUG:
+    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+    SECURE_SSL_REDIRECT = True
+
